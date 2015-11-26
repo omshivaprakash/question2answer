ff935e399a53c4063ed6abb7f87a15623b27a231
Omshivaprakash <shiv@techfiz.info>
2015-11-27 00:53:39 +0530

    Adding more

--- qa-lang/kn-IN/qa-lang-main.php
+++ qa-lang/kn-IN/qa-lang-main.php
@@ -159,7 +159,7 @@
 		'selected' => 'selected',
 		'send_button' => 'Send',
 		'since_x' => 'since ^',
-		'suggest_ask' => 'Help get things started by ^1asking a question^2.',
+		'suggest_ask' => '^1ಪ್ರಶ್ನೆ ಕೇಳುವುದರೊಂದಿಗೆ^2 ಕೆಲಸ ಪ್ರಾರಂಭಿಸಲು ಸಹಾಯಮಾಡಿ.',
 		'suggest_category_qs' => 'To see more, click for all the ^1questions in this category^2.',
 		'suggest_qs' => 'To see more, click for the ^1full list of questions^2.',
 		'suggest_qs_tags' => 'To see more, click for the ^1full list of questions^2 or ^3popular tags^4.',
